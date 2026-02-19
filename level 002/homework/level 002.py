bac1638e586ab1262ed55a49c58465c64fd4f22b
matekvini <matekvinika@gmail.com>
2026-02-19 23:22:25 +0400

    second commit

---
+++ level 002/homework/level 002.py
@@ -0,0 +1,70 @@
+
+# # 2
+ 
+# # string არის მონაცემთა ტიპი რომელშიც შედის ნებისმიერი რამ რაც არის მოთავსებული ბრჭყალებს შორის '' ან ""
+
+# #  integer არის მონაცემთა ტიპი რომელშიც შედის მხოლოდ მთელი რიცხვები
+
+# # float   არის მონაცემთა ტიპი რომელშიც შედის წილადი რიცხვები 
+
+# # 3
+
+# # ცვლასი არის ერთგავერი ყუთი რომელიც იდება რაიმე მნიშვნელობა და მას სახელ ვანიჭებთ მნიშვნელობიდან გამომდინარე 
+
+# # 4
+
+# print("Mate")
+# print("23")
+# print("kvinikadze")
+# print("food")
+# print("favoriti feri")
+
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+
+# print(1.5)
+# print(2.5)
+# print(3.4)
+# print(4.5)
+# print(5.5)
+
+# # 5
+
+# name = "mate"
+# age = 23
+# food = "potato"
+# color = "black"
+# hair_color = "black"
+
+# print(name,age,food,color,hair_color)
+
+# # 6 
+
+# print(1+1)
+# print(2+2)
+# print(3+3)
+
+# print(2-1)
+# print(3-1)
+# print(4-3)
+
+# print(1*1)
+# print(2*2)
+# print(3*3)
+
+# print(1/2)
+# print(2/3)
+# print(3/3)
+
+# 7
+
+num1 = 4
+num2 = 2
+num3 = 1
+ 
+print(num1+num2+num3)
+
--- level 002/homework/შექმენით 5 სხვა დ ასხვა ცვლადი.py
+++
@@ -1,9 +0,0 @@
-
-#სხვა და სხვა ცვლადის შექმნა 
-
-Name = "mate"
-Age =   "23"
-City =  "tbilisi"
-Food =  "kartofili"
-
-print(Name,Age,City,Food)

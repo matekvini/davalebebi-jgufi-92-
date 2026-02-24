db69b6c5a46a6e536db7688ad555c983a99a7228
matekvini <matekvinika@gmail.com>
2026-02-24 22:45:34 +0400

    second commit

---
+++ level 001/classwork/classwork.py
@@ -0,0 +1,11 @@
+ #1
+print("mate")  
+
+#2
+
+print(23)
+
+#3 
+
+print("potato")
+
---
+++ level 003/homework/homework 92.py
@@ -0,0 +1,56 @@
+
+#2  
+
+# კომენტარი არის კოდის ნაწილი რომელსაც პროგრამა აიგნორებს და არ უშვებს როცა კითხულობს. 
+# ის ეხმარება ხალხს გააკეთონ Debugging  ანუ კოდის გამოსწორება მარტივად
+# ის გამოიყენება გუნდურ მუშაობაში რომ დავეხმაროთ გუნდელებს 
+# ის იქმნება # ის შემდეგ ტექსტი 
+
+
+#3 
+# case sensitive  არის კოდის ტიპი თუ კოდის პრიველი ასო იწყება პატარა ასოთი მაშინ კოდი მოგვცემს error -ს.
+# არის მეორე ტიპი   case-insensetive ასეთ კოდის დაწერის შემთხვევაში პრორგამა არ წერს error -ს და აიგნორებს 
+
+#4 
+# snake case არის დასახელების სტილი სადაც სიტყვები ქვედა ტირეებით არის გადაბმული 
+
+# ცვლადების შექმნა Snake Case - ის გამოყენებით
+
+ 
+first_name = "mate"
+favorite_color = "black"
+last_name =   "kvinikadze"
+
+#5 
+
+# Debugging არის შეცდომების bug -ების პოვნის და გამოსწორების პროცესი.
+# bug არის შეცდომა კოდში. ეს აღნიშნავს რომ პროგამა არ მუშაობს ისე, როგორც საჭიროა ან საერთოდ არ ირთვება. 
+
+
+#6 გამოასწორეთ შეცდომები კოდში
+
+
+first_name = "Lika"
+print(first_name)
+
+number = 5
+text = "apples"
+result = str(number) + text
+print(result)
+
+first_name = "liKa"
+second_name = first_name + "4"
+
+first_name = "Lika"
+second_name = "Giorgi"
+print(first_name)
+
+first_name = "Data"
+second_name = "random"
+
+first_name = "Lika"
+second_name = "Giorgi"
+print (first_name)
+
+
+

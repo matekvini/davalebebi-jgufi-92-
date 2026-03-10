89801b5feefa32109843e81ad4e950bc4293f94a
matekvini <matekvinika@gmail.com>
2026-03-10 16:16:59 +0400

    second commit

---
+++ level 004/homework/homework.py
@@ -0,0 +1,33 @@
+#2
+
+num1 = int(input("enter a number "))
+num2 = int(input("enter a number "))
+num3 = int(input("enter a number"))
+
+print((num1+num2+num3)/3)
+
+#3
+num1 = int(input("enter a number "))
+num2 = int(input("enter a number" ))
+print(num1+num2)
+
+#4
+
+mother_name =input("enter your mother name: ")
+father_name =input("enter your father name: ")
+my_name = input("enter your name: ")
+
+print("hello my name is " +my_name + " mother name is "+ mother_name + " and my father name is "+ father_name) 
+
+#5  
+
+user_city = input("enter your city ")
+user_district = input("enter your district ")
+user_floor = input("enter your floor ")
+print(user_city,user_district,user_floor)
+
+#6
+user_age = ("enter your age ")
+print("you are "+ user_age+"years old")
+
+print(f"you are {user_age} years old ")

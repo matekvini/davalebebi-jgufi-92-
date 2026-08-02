5be1c1d28694f1e929f06212b3f6b2c07142fcf1
02 <matekvinika2gmail.com>
2026-08-02 20:43:18 +0400

    second commit

---
+++ levl 026/cw/cw.py
@@ -0,0 +1,1 @@
+# gvqonda code wars 
---
+++ levl 026/hw/hw.py
@@ -0,0 +1,29 @@
+# 1 
+# def number_to_string(num):
+#     return str(num) 
+
+# 2 
+# def solution(string):
+# return string[::-1]
+
+# 3 
+# def make_negative( number ):
+#     if number >0:
+#         return -number
+#     if number <0:
+#         return number
+#     else:
+#         return 0
+
+#4 
+# def bool_to_word(boolean):
+    
+#     if True == boolean:
+#         return "Yes"
+#     if False == boolean:
+#         return "No"
+
+#5
+
+# def opposite(number):
+#     return number * -1
